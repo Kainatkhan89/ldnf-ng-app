49a65f5e7b8f1061ef70614192a114180d7f8cd2
kainat <kainatkhan892014@gmail.com>
2024-07-12 17:08:00 -0300

    refactor learning path service

--- src/app/core/services/learning-path/learning-path.service.ts
+++ src/app/core/services/learning-path/learning-path.service.ts
@@ -1,19 +1,21 @@
 import {inject, Injectable} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {map, Observable, of, tap} from "rxjs";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {from, map, Observable, of, switchMap, tap} from "rxjs";
 import {ILearningPath} from "../../models/learning-path/learning-path.model";
 import { ILearningPathDTO } from '../../models/learning-path/learning-path-dto.model';
-import { ModuleStyles } from '../../models/learning-path/module.model';
 import { LearningPathMapperService } from '../mapper/learning-path-mapper.service';
+import {UserService} from "../user/user.service";
+import {environment} from "../../../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class LearningPathService {
-  private readonly _learningPathApi: string = 'https://localhost:7018/api/course-modules/with-tutorials';
+  private readonly _learningPathApi: string = `${environment.apiUrl}/learning-path`;
 
   private _httpClient: HttpClient = inject(HttpClient);
-  private _learningPathMapperService: LearningPathMapperService = inject(LearningPathMapperService); 
+  private _userService: UserService = inject(UserService);
+  private _learningPathMapperService: LearningPathMapperService = inject(LearningPathMapperService);
   private _cachedLearningPathData: ILearningPath | undefined;
 
   constructor() { }
@@ -27,20 +29,27 @@ export class LearningPathService {
   }
 
   private _fetchLearningPath$(): Observable<ILearningPath> {
-    return this._httpClient.get<ILearningPathDTO>(this._learningPathApi).pipe(
-      map(dto => this._learningPathMapperService.transformDtoToLearningPath(dto)), 
-      tap(value => {
-        console.log(value)
-        if (!this._cachedLearningPathData) {
-          this._cachedLearningPathData = value;
-        } if (!this._cachedLearningPathData) {
-          this._cachedLearningPathData = value;
+    return this._userService.user$.pipe(
+      switchMap(user => {
+        if (user) {
+          return from(user.getIdToken()).pipe(
+            switchMap(token => {
+              const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+              return this._httpClient.get<ILearningPathDTO>(this._learningPathApi, { headers });
+            })
+          );
+        } else {
+          throw new Error('User not authenticated');
         }
       }),
+      map(dto => this._learningPathMapperService.transformDtoToLearningPath(dto)),
+      tap(value => {
+        console.log(value);
+        if (!this._cachedLearningPathData) {
+          this._cachedLearningPathData = value;
+        }
+      })
     );
   }
- 
 }
-
-
 
